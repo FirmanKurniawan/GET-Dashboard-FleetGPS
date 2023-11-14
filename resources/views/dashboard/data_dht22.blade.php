6408292334172685e039a5b4eaf9d13c539f5fd4
FirmanKurniawan <kurniawan.firman94@gmail.com>
2023-11-14 22:52:59 +0700

    Add Table

--- resources/views/dashboard/data_dht22.blade.php
+++ resources/views/dashboard/data_dht22.blade.php
@@ -329,7 +329,7 @@
             </div>
         </div>
 
-        v class="row">
+        <div class="row">
             <div class="col-lg-12 col-md-12 col-12 col-sm-12">
             <div class="card">
                 <div class="card-header">
@@ -384,7 +384,7 @@
             </div>
         </div>
 
-        v class="row">
+        <div class="row">
             <div class="col-lg-12 col-md-12 col-12 col-sm-12">
             <div class="card">
                 <div class="card-header">
